0ab01f5511a7ee75ace4c2f18e1af81d12628b6b
Grégoire HUBERT <gregoire.hubert.pro@gmail.com>
2011-05-19 16:26:31 +0200

    Array support

--- Pomm/Tools/CreateBaseMapTool.php
+++ Pomm/Tools/CreateBaseMapTool.php
@@ -144,7 +144,19 @@ EOD;
         foreach ($this->attributes as $attribute)
         {
             $field_name = $attribute['attname'];
-            $field_type = $this->connection->getDatabase()->getConverterNameForType($attribute['format_type']);
+
+            if (preg_match('/^([\w ])+\[\]$/', $attribute['format_type'], $matchs))
+            {
+                $array_modifier = '[]';
+                $format_type = $matchs[1];
+            }
+            else
+            {
+                $array_modifier = '';
+                $format_type = $attribute['format_type'];
+            }
+
+            $field_type = $this->connection->getDatabase()->getConverterNameForType($format_type).$array_modifier;
 
             $fields_definition .= sprintf("        \$this->addField('%s', '%s');\n", $field_name, $field_type);
         }
