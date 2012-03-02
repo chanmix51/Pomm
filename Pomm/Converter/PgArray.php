1ecd68e5f9745b9ab5eee0e4917d0e713f241fc5
Grégoire HUBERT <hubert.greg@gmail.com>
2012-03-02 16:02:55 +0100

    problem with empty sets using pgarray
        empty pg arrays now returns empty php arrays.

--- Pomm/Converter/PgArray.php
+++ Pomm/Converter/PgArray.php
@@ -40,7 +40,7 @@ class pgArray implements ConverterInterface
                 ->getConverterForType($type);
 
             return array_map(function($val) use ($converter, $type) {
-                    return $converter->fromPg($val, $type);
+                    return $converter->fromPg(str_replace('\\"', '"', $val), $type);
                 },
                     preg_split('/[,\s]*"((?:[^\\\\"]|\\\\.|"")+)"[,\s]*|[,\s]+/', 
                         str_replace('""', '"', trim($data, "{}")), 0, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE));
