0e84b9a1598014e89b8cbf53bedd85cabb71aa3c
Grégoire HUBERT <hubert.greg@gmail.com>
2012-07-03 22:31:59 +0200

    Typo in smart identity mapper
        A Typo was preventing the identity mapper to check correctly the
        instance was kept in memory.

--- Pomm/Identity/IdentityMapperSmart.php
+++ Pomm/Identity/IdentityMapperSmart.php
@@ -40,14 +40,14 @@ class IdentityMapperSmart extends IdentityMapperStrict
     /**
      * @see Pomm\Identity\IdentityMapperInterface.
      **/
-    public function checkModelInstance($class_name, Array $primary_key)
+    public function checkModelInstance($class_name, Array $pk_fields)
     {
         if (count($pk_fields) == 0)
         {
             return $object;
         }
 
-        $crc = $this->getSignature($class_name, $primary_key);
+        $crc = $this->getSignature($class_name, $pk_fields);
 
         return array_key_exists($crc, $this->mapper) && ($this->mapper[$crc]->_getStatus() & BaseObject::EXIST)
             ? $this->mapper[$crc]
