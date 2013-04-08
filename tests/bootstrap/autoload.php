45f015d5abe36949483a8ccd07168fa5fb3c3519
Adrien Brault <adrien.brault@gmail.com>
2013-04-08 21:30:21 +0200

    Use composer autoloading for tests

--- tests/bootstrap/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-spl_autoload_register(function ($class) {
-    if (0 === strpos($class, 'Pomm\\')) {
-        $class = str_replace('\\', '/', $class);
-        require sprintf("%s/%s.php", dirname(dirname(__DIR__)), $class);
-    }
-});
